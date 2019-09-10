26984ae5a100a21211a7c34be82edcb244a97c78
Logon Aluno <logonaluno@local.com>
2019-09-10 11:00:36 -0300

    Corrigindo erro do CORS

--- src/main/java/br/com/bev/config/CorsFilter.java
+++ src/main/java/br/com/bev/config/CorsFilter.java
@@ -1,7 +1,23 @@
 package br.com.bev.config;
 
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
-public class CORS implements ContainerResponseFilter{
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
+@Component
+@Order(1)
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        chain.doFilter(req, response);
+    }
 }
