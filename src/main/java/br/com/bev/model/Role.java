f4eba3615ab0454525fea2ab5dc696e613858a90
Logon Aluno <logonaluno@local.com>
2019-10-17 11:58:32 -0300

    Adicionando JoinColumn

--- src/main/java/br/com/bev/model/Role.java
+++ src/main/java/br/com/bev/model/Role.java
@@ -4,8 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.*;
 
 @Entity
